1a85f8627f5d055b646ba3d1f1f8972c6ea198c7
Kudzanayi Dzvairo <kudzanayi@Kudzanayis-MacBook-Air.local>
2018-12-01 04:11:42 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
-# your code goes here
+require 'pry'
+
+def begins_with_r(array)
+    array.all? do |word|
+    word.start_with?("r")
+    end
+end
+
+def contain_a(array)
+
+    array.find_all do |element|
+    element.include?('a')
+  end
+end
+
+def first_wa(array)
+  array.find do |word|
+    word.to_s.start_with?("wa")
+  end
+end
+
+def remove_non_strings(array)
+    array.delete_if do |element|
+      element.class != String
+    end
+end
+
+def count_elements(array)
+  array.count do |element|
+    name > 0
+  end
+end
